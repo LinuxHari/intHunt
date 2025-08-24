a72a427487e53cb96db30c0204f4ddfdb331077d
Linux Hari <vadiveluversionz1.0@gmail.com>
2025-08-24 22:59:12 +0530

    feat: types

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,6 +11,8 @@ interface Feedback {
   areasForImprovement: string[];
   finalAssessment: string;
   createdAt: Date;
+  role: string;
+  type: Interview["type"];
 }
 
 interface BaseInterview {
@@ -135,3 +137,141 @@ interface InterviewFormProps {
 interface TechIconProps {
   techStack: string[];
 }
+interface CatchReturn {
+  success: false;
+  message: unknown;
+}
+
+type ReturnPublishedInterviews = {
+  success: true;
+  publishedInterviews: Array<PublishedInterview>;
+  totalCounts: number;
+};
+
+interface AnalyticsData {
+  period: string;
+  totalAttendedInterviews: number;
+  averageScore: number;
+  averageQuestions: number;
+}
+
+interface AnalyticsQueryRow {
+  time_frame: "current" | "previous";
+  period: string;
+  total_attended_interviews: number | null;
+  average_score: number | null;
+  average_questions: number | null;
+}
+
+interface ReturnUserAnalytics {
+  success: true;
+  analytics: {
+    currentPeriod: AnalyticsData[];
+    previousPeriod: AnalyticsData[];
+  };
+}
+
+type Duration = "week" | "month" | "year";
+
+type ReturnUserRecents = {
+  success: true;
+  recents: Array<{
+    role: string;
+    attendedAt: Date;
+    type: Interview["type"];
+    score: number;
+  }>;
+};
+
+type ReturnUpcoming = {
+  success: true;
+  upcomingStats: {
+    totalScheduled: number;
+    // dueToday: number;
+    averageQuestions: number;
+  };
+};
+
+type ReturnPublished = {
+  success: true;
+  publishedStats: {
+    totalPublished: number;
+    totalAttendees: number;
+    active: number;
+  };
+};
+
+interface ReturnAttended {
+  success: true;
+  attendedStats: {
+    averageScore: number;
+    averageQuestions: number;
+    completed: number;
+  };
+}
+
+interface ReturnProfile {
+  success: true;
+  profileStats: {
+    interviewsTaken: number;
+    interviewsCreated: number;
+    memberSince: string;
+    averageScore: number;
+  };
+}
+
+interface ReturnUpcomingInterviews {
+  success: true;
+  upcomingInterviews: UpcomingInterview[];
+  totalCounts: number;
+}
+
+interface AttendedInterviewData {
+  userId: string;
+  interviewId: string;
+  score: number;
+  feedback: string;
+  attendedAt: string;
+  level: Interview["level"];
+  type: Interview["type"];
+}
+
+interface AttendedInterviewDoc extends AttendedInterviewData {
+  id: string;
+}
+
+interface AttendedInterviewDetailsData {
+  type: Interview["type"];
+  level: Interview["level"];
+  difficulty: Interview["difficulty"];
+  role: string;
+  techStack: string[];
+}
+
+interface ReturnAttendedInterviews {
+  success: boolean;
+  attendedInterviews: AttendedInterview[];
+  totalCounts: number;
+}
+
+interface InterviewSearchParams {
+  query: string;
+  sortType?: "rating" | "attendees" | "questions";
+  interviewType?: Interview["type"] | "all";
+  page?: number;
+  offset?: number;
+}
+
+type ReturnInterviewSearch = {
+  success: true;
+  interviews: Interview[];
+  totalCount: number;
+  hasNextPage: boolean;
+};
+
+type ScheduleDetails = {
+  interviewId: string;
+  date: string;
+  time: string;
+  timezone: string;
+};
