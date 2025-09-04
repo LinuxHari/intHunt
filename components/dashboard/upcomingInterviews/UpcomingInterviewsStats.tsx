aace8dd0a63c197df81e5adb86dc429977b6f620
Linux Hari <vadiveluversionz1.0@gmail.com>
2025-09-05 00:32:01 +0530

    feat: duration

--- components/dashboard/upcomingInterviews/UpcomingInterviewsStats.tsx
+++ components/dashboard/upcomingInterviews/UpcomingInterviewsStats.tsx
@@ -1,6 +1,6 @@
 import React, { use } from "react";
 import StatCard from "./StatCard";
-import { Calendar, Play } from "lucide-react";
+import { Calendar, Clock, Play } from "lucide-react";
 
 interface UpcomingInterviewsStatsProps {
   upcomingInterviewsStats: Promise<ReturnUpcoming | CatchReturn>;
@@ -13,6 +13,13 @@ const UpcomingInterviewsStats = ({
 
   if (!stats.success) return null;
 
+  const totalDuration =
+    Math.ceil(
+      stats.upcomingStats.averageQuestions *
+        stats.upcomingStats.totalScheduled *
+        90
+    ) / 60;
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
       <StatCard
@@ -21,17 +28,17 @@ const UpcomingInterviewsStats = ({
         label="Total Scheduled"
         bgColor="bg-blue-100 dark:bg-blue-900 text-blue-600 dark:text-blue-400"
       />
-      {/* <StatCard
-        icon={Clock}
-        value={stats.upcomingStats.dueToday}
-        label="Due Today"
-        bgColor="bg-green-100 dark:bg-green-900 text-green-600 dark:text-green-400"
-      /> */}
       <StatCard
         icon={Play}
         value={stats.upcomingStats.averageQuestions}
         label="Average Questions"
         bgColor="bg-purple-100 dark:bg-purple-900 text-purple-600 dark:text-purple-400"
+      />
+      <StatCard
+        icon={Clock}
+        value={totalDuration}
+        label="Expected Duration (minutes)"
+        bgColor="bg-green-100 dark:bg-green-900 text-green-600 dark:text-green-400"
       />
     </div>
   );
